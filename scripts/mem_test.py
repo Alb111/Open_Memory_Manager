10eeefce33ca5de90d48f5e778d2bce210a082c7
Saanj Shetty <saanj135@gmail.com>
2026-01-28 23:53:51 -0800

    basic mem ctrl tests

---
+++ scripts/mem_test.py
@@ -0,0 +1,15 @@
+from mem_ctrl import MemCtrl
+
+mc = MemCtrl()
+
+mc.write_inst(0x00, 0xDEADBEEF)
+mc.write_inst(0x01, 0xC0FFEEFF)
+mc.write_inst(0x03, 0xCAFECAFE)
+mc.write_inst(0x06, 0xABCDEFEF)
+mc.write_inst(0x08, 0xACDCABBA)
+
+print(hex(mc.read_inst(0x00)))
+print(hex(mc.read_inst(0x01)))
+print(hex(mc.read_inst(0x02)))
+print(hex(mc.read_inst(0x03)))
+print(hex(mc.read_inst(0x08)))
