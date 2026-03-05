1cd009cf391622846703b521d5c5e1f09a1f411c
Albert Felix <albertfelix2005@gmail.com>
2026-03-04 17:12:04 -0800

    oops

--- cocotb/wrr_arbiter_test.py
+++ cocotb/wrr_arbiter_test.py
@@ -1,10 +1,20 @@
 # test_wrr_arbiter.py
+import os
+from pathlib import Path
 
 import cocotb
 from cocotb.triggers import RisingEdge, Timer
 from cocotb.clock import Clock
-from cocotb.result import TestFailure
+# from cocotb.result import TestFailure
+from cocotb_tools.runner import get_runner
 import random
+
+sim = os.getenv("SIM", "icarus")
+pdk_root = os.getenv("PDK_ROOT", Path("~/.ciel").expanduser())
+pdk = os.getenv("PDK", "gf180mcuD")
+scl = os.getenv("SCL", "gf180mcu_fd_sc_mcu7t5v0")
+gl = os.getenv("GL", False)
+slot = os.getenv("SLOT", "1x1")
 
 # Python Reference Model 
 class WeightedRoundRobinModel:
@@ -48,9 +58,9 @@ class WeightedRoundRobinModel:
 
 
 # Utility Functions
-
 def onehot_to_list(val, width=2):
     return [(val >> i) & 1 for i in range(width)]
+
 
 
 def list_to_int(lst):
@@ -62,18 +72,16 @@ def list_to_int(lst):
 
 
 # Reset Sequence
-
 async def reset_dut(dut):
-    dut.rst_n.value = 0
-    dut.req.value = 0
+    dut.rst_ni.value = 0
+    dut.req_i.value = 0
     for _ in range(5):
-        await RisingEdge(dut.clk)
-    dut.rst_n.value = 1
-    await RisingEdge(dut.clk)
+        await RisingEdge(dut.clk_i)
+    dut.rst_ni.value = 1
+    await RisingEdge(dut.clk_i)
 
 
 # Main Test
-
 @cocotb.test()
 async def test_weighted_round_robin(dut):
 
@@ -81,7 +89,7 @@ async def test_weighted_round_robin(dut):
     WEIGHTS = [3, 1]   # Change freely for testing
 
     # Start clock
-    clock = Clock(dut.clk, 10, units="ns")
+    clock = Clock(dut.clk_i, 10, unit="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -97,19 +105,23 @@ async def test_weighted_round_robin(dut):
 
     dut._log.info("Running directed weighted test")
 
-    dut.req.value = 0b11
+    dut.req_i.value = 0b11
 
     for cycle in range(20):
-        await RisingEdge(dut.clk)
+        await RisingEdge(dut.clk_i)
 
-        dut_grant = onehot_to_list(int(dut.grant.value), NUM_REQ)
+        dut_grant = onehot_to_list(int(dut.grant_o.value), NUM_REQ)
         expected_grant = model.arbitrate([1, 1])
 
-        if dut_grant != expected_grant:
-            raise TestFailure(
-                f"Mismatch at cycle {cycle}: "
-                f"DUT={dut_grant} Expected={expected_grant}"
-            )
+        # if dut_grant != expected_grant:
+        #     raise TestFailure(
+        #         f"Mismatch at cycle {cycle}: "
+        #         f"DUT={dut_grant} Expected={expected_grant}"
+        #     )
+         
+        assert(dut_grant == expected_grant), f"Mismatch at cycle {cycle}: \n DUT={dut_grant} Expected={expected_grant}"
+
+        
 
     dut._log.info("Directed weighted test PASSED")
 
@@ -117,23 +129,58 @@ async def test_weighted_round_robin(dut):
 
     dut._log.info("Running randomized request test")
 
+    dut._log.info(" hellllllllllllllllo")
     for cycle in range(100):
 
         # Random request pattern
         req_vec = [random.randint(0, 1) for _ in range(NUM_REQ)]
-        dut.req.value = list_to_int(req_vec)
+        dut.req_o.value = list_to_int(req_vec)
 
-        await RisingEdge(dut.clk)
+        await RisingEdge(dut.clk_i)
 
-        dut_grant = onehot_to_list(int(dut.grant.value), NUM_REQ)
+
+        # dut._log.info(f" hellllllllllllllllo {dut.grant_o.value}")
+        dut_grant = onehot_to_list(int(dut.grant_o.value), NUM_REQ)
         expected_grant = model.arbitrate(req_vec)
 
-        if dut_grant != expected_grant:
-            raise TestFailure(
-                f"Random Test Mismatch at cycle {cycle}: "
-                f"REQ={req_vec} DUT={dut_grant} Expected={expected_grant}"
-            )
+        # if dut_grant != expected_grant:
+        #     raise TestFailure(
+        #         f"Random Test Mismatch at cycle {cycle}: "
+        #         f"REQ={req_vec} DUT={dut_grant} Expected={expected_grant}"
+        #     )
+
+        assert(dut_grant == expected_grant), f"Mismatch at cycle {cycle}: \n DUT={dut_grant} Expected={expected_grant}"
 
     dut._log.info("Randomized test PASSED")
-
     dut._log.info("All WRR tests PASSED successfully")
+
+def wrr_arbiter_runner():
+    proj_path = Path(__file__).resolve().parent
+
+    sources = [
+        proj_path / "../src/arb/wrr_arbiter.sv",
+    ]
+
+    build_args = []
+    if sim == "icarus":
+        pass
+    if sim == "verilator":
+        build_args = ["--timing", "--trace", "--trace-fst", "--trace-structs"]
+    
+    runner = get_runner(sim)
+    runner.build(
+        sources=sources,
+        hdl_toplevel="wrr_arbiter",
+        always=True,
+        build_args=build_args,
+        waves=True,
+    )
+
+    runner.test(
+        hdl_toplevel="wrr_arbiter",
+        test_module="wrr_arbiter_test",
+        waves=True,
+    )
+
+if __name__ == "__main__":
+    wrr_arbiter_runner()
