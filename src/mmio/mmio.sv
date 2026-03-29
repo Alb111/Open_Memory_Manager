100ebdcecb0ddd28972a86873dd6a0440dc6db28
Saanj Shetty <saanj135@gmail.com>
2026-03-29 00:10:06 -0700

    port naming errors

--- src/mmio/mmio.sv
+++ src/mmio/mmio.sv
@@ -6,7 +6,7 @@ module mmio (
     input logic [31:0] addr_i,
     input logic [31:0] wr_data_i,
     input logic wr_en_i, // write enable
-    output logic [31:0] rdata_o,     // read data back to cpu
+    output logic [31:0] rd_data_o,     // read data back to cpu
 
     // physical connections to the serializer/pins
     output logic [7:0] gpio_pins_o, // data going out
@@ -23,15 +23,15 @@ module mmio (
     localparam ADDR_CSR = 32'h8000_0018;
 
     // write
-    always_ff @(posedge clk or negedge rst_n) begin
-        if(!rst_n) begin
+    always_ff @(posedge clk_i or negedge rst_in) begin
+        if(!rst_in) begin
             data_reg <= 8'h00;
             csr_reg <= 8'h00; // default all to inputs
-        end else if(wen_i) begin
+        end else if(wr_en_i) begin
             if(addr_i == ADDR_DATA) begin
-                data_reg <= wdata_i[7:0];
+                data_reg <= wr_data_i[7:0];
             end else if(addr_i == ADDR_CSR) begin
-                csr_reg <= wdata_i[7:0];
+                csr_reg <= wr_data_i[7:0];
             end
         end
     end
@@ -39,11 +39,11 @@ module mmio (
     //read
     always_comb begin
         if(addr_i == ADDR_DATA) begin
-            rdata_o = {24'h0, data_reg};
+            rd_data_o = {24'h0, data_reg};
         end else if(addr_i == ADDR_CSR) begin
-            rdata_o = {24'h0, csr_reg};
+            rd_data_o = {24'h0, csr_reg};
         end else begin
-            rdata_o = 32'h0;
+            rd_data_o = 32'h0;
         end
     end
 
