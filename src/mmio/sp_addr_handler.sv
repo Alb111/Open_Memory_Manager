39a317400c1e609b81267af727f2ddeeea0494c5
Jack Wong <jac.wong.k@gmail.com>
2026-04-28 15:29:37 -0700

    cleaned up interfaces, and edited some of the logic. Untested.

--- src/mmio/sp_addr_handler.sv
+++ src/mmio/sp_addr_handler.sv
@@ -1,36 +1,39 @@
 `timescale 1ns/1ps
 
-module sp_addr_handler #(
-    parameter int WHOAMI_ID = 32'hA1B2_C3D4 // ID
-)(
-    input logic clk_i,
-    input logic rst_ni,
+module sp_addr_handler #()(
+    input         clk_i,
+    input         rst_ni,
 
-    //interface from cpu /system bus
-    input logic [31:0] addr_i,
-    input logic [31:0] wr_data_i,
-    input logic wr_en_i,      // write enable (1 = write, 0 =read)
-    output logic [31:0] rd_data_o,     //data sent back to cpu
-    output logic ack_o,       //addr acknowledged
+    //interface from cpu (native picorv32)
+    input         mem_valid
+    output        mem_ready
 
-    //interface to rest of chip (passthrough)
-    output logic [31:0] passthru_addr_o,
-    output logic [31:0] passthru_wr_data_o,
-    output logic passthru_wr_en_o,
-    input logic [31:0] passthru_rd_data_i,
+    input  [31:0] mem_addr
+    input  [31:0] mem_wdata
+    input  [ 3:0] mem_wstrb
+    output [31:0] mem_rdata
 
-    //pin connections
-    output logic [7:0] gpio_pins_o,
-    input logic [7:0] gpio_pins_i,
-    output logic [7:0] gpio_dir_o
+    //downstream passthrough interface
+    output        pass_mem_valid
+    input         pass_mem_ready
+
+    output [31:0] pass_mem_addr
+    output [31:0] pass_mem_wdata
+    output [ 3:0] pass_mem_wstrb
+    input  [31:0] pass_mem_rdata
+
+    //gpio pin connections
+    output [ 7:0] gpio_pins_o,
+    input  [ 7:0] gpio_pins_i,
+    output [ 7:0] gpio_dir_o
+
+    //cpu_id
+    input  [ 7:0] cpu_id_i
 );
-
-    //internal signals to talk to mmio block
-    logic [31:0] mmio_rd_data;
-    logic is_special_addr;
 
     //addr decoding
     //check if addr starts with 0x8000
+    logic is_special_addr;
     always_comb begin
         if((addr_i & 32'hFFFF_0000) == 32'h8000_0000) begin
             is_special_addr = 1'b1;
@@ -39,37 +42,40 @@ module sp_addr_handler #(
         end
     end
 
-    //routing
-    // if special addr then block it from going to rest of chip
-    assign passthru_addr_o = (is_special_addr) ? 32'h0 : addr_i;
-    assign passthru_wr_en_o = (is_special_addr) ? 1'b0 : wr_en_i;
-    assign passthru_wr_data_o = wr_data_i;
-
     //handling whoami and mmio reads
+    logic [31:0] mmio_rd_data;
     always_comb begin
         if(is_special_addr) begin
-            ack_o = 1'b1; //claim this address
             if(addr_i == 32'h8000_0000) begin
-                rd_data_o = WHOAMI_ID; // return chips unique ID
+                mem_rdata = {24'b0, cpu_id_i}; // return chips unique ID
             end else begin
-                rd_data_o = mmio_rd_data; //return data from the mmio regs
+                mem_rdata = mmio_rd_data; //return data from the mmio regs
             end
         end else begin
-            ack_o = 1'b0;
-            rd_data_o = passthru_rd_data_i; // passhtru data from memory
+            mem_rdata = pass_mem_rdata; // passhtru data from memory
         end
     end
+
+    logic mmio_wr_en;
+    assign mmio_wr_en = |mem_wstrb & is_special_addr & mem_valid;
 
     mmio mmio_inst (
         .clk_i(clk_i),
         .rst_ni(rst_ni),
-        .addr_i(addr_i),
-        .wr_data_i(wr_data_i),
-        .wr_en_i(wr_en_i && is_special_addr), //only write if its a special addr
+        .addr_i(mem_addr),
+        .wr_data_i(mem_wdata),
+        .wr_en_i(mmio_wr_en), //only write if its a special addr
         .rd_data_o(mmio_rd_data),
         .gpio_pins_o(gpio_pins_o),
         .gpio_pins_i(gpio_pins_i),
         .gpio_dir_o(gpio_dir_o)
     );
+
+    // passthrough but only validate if not sp addr
+    assign pass_mem_addr = mem_addr;
+    assign pass_mem_wdata = mem_wdata;
+    assign pass_mem_wstrb = mem_wstrb;
+    assign pass_mem_valid = ~is_special_addr & mem_valid;
+    assign mem_ready = pass_mem_ready | is_special_addr;
 
 endmodule
