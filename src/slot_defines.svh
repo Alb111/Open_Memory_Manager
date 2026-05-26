26273f618314b85b5adf6b6d3e2c116c2e794ce2
Nicholas Gaither <nicholas.gaither@gmail.com>
2026-05-25 17:31:09 -0700

    prepare pins

--- src/slot_defines.svh
+++ src/slot_defines.svh
@@ -1,13 +1,13 @@
 `ifdef SLOT_1X1
 
 // Power/ground pads for core and I/O
-`define NUM_DVDD_PADS 8
+`define NUM_DVDD_PADS 10
 `define NUM_DVSS_PADS 10
 
 // Signal pads
-`define NUM_INPUT_PADS 12
+`define NUM_INPUT_PADS 4
 `define NUM_BIDIR_PADS 40
-`define NUM_ANALOG_PADS 2
+`define NUM_ANALOG_PADS 8
 
 `endif
 
